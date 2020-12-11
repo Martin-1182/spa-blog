b7a874ea54473c0ccd5572797fc2c411b270ed5a
Martin Halaj <m.halaj@websystem.sk>
2020-12-11 08:32:25 +0100

    Add bootstrap navbar

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,30 +1,65 @@
 <template>
-	<div class="navigation">
-		<div class="logo">
-			<img alt="Vue logo" src="../assets/logo.png" />
+	<nav class="navbar navbar-expand-lg navbar-light">
+		<router-link class="navbar-brand" to="/">
+			<img alt="Vue logo" src="../assets/logo.png" height="50" />
+		</router-link>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-toggle="collapse"
+			data-target="#navbarNavDropdown"
+			aria-controls="navbarNavDropdown"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item dropdown active">
+					<router-link
+						class="nav-link dropdown-toggle text-uppercase font-weight-bold"
+						to="/"
+						id="navbarDropdownMenuLink"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false"
+					>
+						Home<span class="sr-only">(current)</span>
+					</router-link>
+					<div
+						class="dropdown-menu"
+						aria-labelledby="navbarDropdownMenuLink"
+					>
+						<router-link to="#"></router-link>
+						<router-link to="#"></router-link>
+						<router-link to="#"></router-link>
+					</div>
+				</li>
+				<li class="nav-item">
+					<router-link
+						class="nav-link text-uppercase font-weight-bold"
+						to="/About"
+						>About</router-link
+					>
+				</li>
+				<li class="nav-item">
+					<router-link
+						to="/blog"
+						class="nav-link text-uppercase font-weight-bold"
+						>Blog</router-link
+					>
+				</li>
+				<li class="nav-item">
+					<router-link
+						to="/contact"
+						class="nav-link text-uppercase font-weight-bold"
+						>Contact</router-link
+					>
+				</li>
+			</ul>
 		</div>
-		<div class="links">
-			<router-link to="/"
-				>Home
-				<svg
-					class="chevron-down"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M19 9l-7 7-7-7"
-					/></svg
-			></router-link>
-			<router-link to="/about">About</router-link>
-			<router-link to="/Blog">Blog</router-link>
-			<router-link to="/contact">Contact</router-link>
-		</div>
-	</div>
+	</nav>
 </template>
 
 <script>
